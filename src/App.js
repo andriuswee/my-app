ebcea2eed37e6cff15cf604a843b151b41cbabed
andriuswee <104457683+andriuswee@users.noreply.github.com>
2022-10-04 20:04:50 +0300

    Stage 4 (inc/dec func)

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ const secondButtonTitle = "Second button";
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => setCount(count + 1);
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
 
   return (
     <div>
       <h1>{title}</h1>
 
-      <MyButton onClickProp={handleClick} text="First button" />
+      <MyButton onClickProp={handleIncrement} text="Increment counter" />
 
-      <MyButton onClickProp={handleClick} text={secondButtonTitle} />
+      <MyButton onClickProp={handleDecrement} text="Increment counter" />
 
       <h1>Click so many times: {count}</h1>
     </div>
