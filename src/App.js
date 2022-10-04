4ad158b0b09c7d4bee2405587895e146d3f12da4
andriuswee <104457683+andriuswee@users.noreply.github.com>
2022-10-04 18:55:36 +0300

    Stage2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import MyButton from "./Components/MyButton";
+
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
       <button>My react button</button>
+      <MyButton></MyButton>
     </div>
   );
 }
---
+++ src/Components/MyButton.js
@@ -0,0 +1,3 @@
+const MyButton = () => <button>My created button</button>;
+
+export default MyButton;
